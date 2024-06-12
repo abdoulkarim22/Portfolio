f0633d26e35883595d93e5a5f92a1aa57f2bb699
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-06-12 14:25:57 +0100

    edit the icon social media

--- src/components/Socialicon.jsx
+++ src/components/Socialicon.jsx
@@ -24,24 +24,17 @@ function Socialicon() {
       {/* twitter  */}
       <a
         href="https://x.com/Abdoulk61277237"
-        className=" rounded-lg bg-slate-200 inline-block hover:bg-blue-500"
+        className=" rounded-lg bg-slate-200 inline-block hover:bg-black-500"
       >
-        <svg
-          className="w-10 h-10 p-2.5  text-2xl fill-blue-400 hover:fill-white"
-          xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          viewBox="0 0 512 512"
-        >
-          <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
-        </svg>
+        <svg className="w-10 h-10 p-2.5  text-2xl fill-blue-400 hover:fill-white" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="currentColor"><path d="M1 2h2.5L3.5 2h-2.5z"><animate fill="freeze" attributeName="d" dur="0.4s" values="M1 2h2.5L3.5 2h-2.5z;M1 2h2.5L18.5 22h-2.5z"/></path><path d="M5.5 2h2.5L7.2 2h-2.5z"><animate fill="freeze" attributeName="d" dur="0.4s" values="M5.5 2h2.5L7.2 2h-2.5z;M5.5 2h2.5L23 22h-2.5z"/></path><path d="M3 2h5v0h-5z" opacity="0"><set attributeName="opacity" begin="0.4s" to="1"/><animate fill="freeze" attributeName="d" begin="0.4s" dur="0.4s" values="M3 2h5v0h-5z;M3 2h5v2h-5z"/></path><path d="M16 22h5v0h-5z" opacity="0"><set attributeName="opacity" begin="0.4s" to="1"/><animate fill="freeze" attributeName="d" begin="0.4s" dur="0.4s" values="M16 22h5v0h-5z;M16 22h5v-2h-5z"/></path><path d="M18.5 2h3.5L22 2h-3.5z" opacity="0"><set attributeName="opacity" begin="0.5s" to="1"/><animate fill="freeze" attributeName="d" begin="0.5s" dur="0.4s" values="M18.5 2h3.5L22 2h-3.5z;M18.5 2h3.5L5 22h-3.5z"/></path></g></svg>
       </a>
 
       {/*github  */}
       <a
         href="https://github.com/abdoulkarim22?tab=repositories"
-        className=" rounded-lg bg-slate-200 inline-block hover:bg-blue-500"
+        className=" rounded-lg bg-slate-200 inline-block hover:bg-black-500"
       >
-        <svg className="w-10 h-10 p-2.5 fill-black hover:fill-white"
+        <svg className="w-10 h-10 p-2.5 fill-black hover:fill-green"
           xmlns="http://www.w3.org/2000/svg"
           height="1em"
           viewBox="0 0 496 512"
